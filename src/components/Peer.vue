dbc27d4c16f54479d1fbcfe501a10aae8d204e05
Phelan Lewis <phelanlewis@gmail.com>
2018-05-19 23:30:42 -0700

    Added peer message form with vue functions

--- src/components/Peer.vue
+++ src/components/Peer.vue
@@ -1,18 +1,45 @@
 <template>
-  <h1>This is the Peer Video Channel</h1>
+  <div>
+    <h1>This is the Peer Video Channel</h1>
+    <form>
+      <textarea id="incoming"></textarea>
+      <button v-on:click="sendMessage">submit</button>
+    </form>
+    <pre id="outgoing"></pre>
+  </div>
 </template>
 <script>
 const Peer = require('simple-peer')
 const peer = Peer()
 
-peer.on('signal', (data) => {
-  console.log(data)
+peer.on('error', function (err) { console.log('error', err) })
+
+peer.on('signal', function (data) {
+  console.log('SIGNAL', JSON.stringify(data))
+  document.querySelector('#outgoing').textContent = JSON.stringify(data)
+})
+
+peer.on('connect', function () {
+  console.log('CONNECT')
+  peer.send('whatever' + Math.random())
+})
+ 
+peer.on('data', function (data) {
+  console.log('data: ' + data)
 })
 
 export default {
-
+ methods: {
+    sendMessage: function (event) {
+      //peer.signal(JSON.parse(document.querySelector('#incoming').value))
+      console.log("I am the the event", event)
+    }
+  } 
 }
 </script>
 <style>
-
+  #outgoing {
+        width: 600px;
+        word-wrap: break-word;
+      }
 </style>
